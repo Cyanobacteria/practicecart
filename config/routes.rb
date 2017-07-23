7d8b2c2b2a7bc6deb2840388e6e2b4ef565fcd91
cyanobacteria <vodomelkor@gmail.com>
2017-07-24 00:35:26 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :statics , :only =>[:index]
+  root "statics#index"
+  resources :item ,:only => [:index , :show]
+
+  namespace :dashboard do
+    
+    resources :items
+    namespace :hahaadmon do
+      resources :items
+      resources :cates
+      resources :orders
+      resources :users
+      resources :manager
+
+    end  
+  end
+
+
 end
